29bf835287c11055a8be373b2a6ed6b0ced73b65
masahiro331 <lomycisw@gmail.com>
2017-11-05 19:36:49 +0900

    first commit

---
+++ sample/example_with.exs
@@ -0,0 +1,8 @@
+# mean  = with count = Enum.count(values),
+#              sum = Enum.sum(values)
+#         do
+#           sum/count
+#         end
+# mean = with count = Enum.count(values),
+#             sum = Enum.sum(values),
+#        do: sum/count
